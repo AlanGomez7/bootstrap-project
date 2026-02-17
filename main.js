5a79adec6b4104907942477c63612702edd105a4
modelctzn <gary@socialsense.co.uk>
2026-02-18 00:08:23 +0530

    images and cards added

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 const addItems = () => {
-    console.log("running")
-    const items = ["Vada.avif", "Upma.avif", "Tea.avif", "Shake.avif", "Juice.avif", "Idli.avif",
-        "Dosa.avif", "Dhokla.avif", "Chole-Bhature.avif", "Cake.avif"
-    ]
-    const parent = document.getElementById('option-box');
-    parent.innerHTML = "";
-    items.forEach(element => {
-        console.log(element)
-        parent.innerHTML +=
-            `<div class="option-img" style="background-image:url(images/${element})">
-            </div>`
-    });
-}
+  const items = [
+    "Vada.avif",
+    "Upma.avif",
+    "Tea.avif",
+    "Shake.avif",
+    "Juice.avif",
+    "Idli.avif",
+    "Dosa.avif",
+    "Dhokla.avif",
+    "Chole-Bhature.avif",
+    "Cake.avif",
+  ];
 
-document.addEventListener("DOMContentLoaded", () => {
-    addItems();
-});
+  const parent = document.getElementById("option-box");
+  parent.innerHTML = "";
+
+  items.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("option-img");
+    div.style.backgroundImage = `url(images/${item})`;
+    parent.appendChild(div);
+  });
+};
+
+const addRestaurants = () => {
+  const parent = document.getElementById("resto-list");
+
+  [{image:"Cake.avif", title:"Cake", description: "Hello"}].forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card shadow-sm";
+    card.style.width = "18rem";
+
+    card.innerHTML = `
+      <img src="/images/${item.image}" class="card-img-top" alt="${item.title}" />
+      <div class="card-body">
+        <h5 class="card-title">${item.title}</h5>
+        <p class="card-text">${item.description}</p>
+        <a href="#" class="btn btn-primary">View Menu</a>
+      </div>
+    `;
+
+    parent.appendChild(card);
+  });
+};
+
+window.onload = addItems;
